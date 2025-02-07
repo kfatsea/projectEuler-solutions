46be4c6d36c9ea232a9fb21da779e11c6a7af83e
kfatsea <fatseakona@gmail.com>
2025-02-07 18:12:04 +0100

    refactor(problem-001/python): maintenance
    Added docstrings and description for readability

--- problem-001/initial_solution/brute_force.py
+++ problem-001/initial_solution/brute_force.py
@@ -1,32 +1,51 @@
-'''
-Algorithm: SumMultiplesOf3And5 - Brute Force
-Input: An integer `n`
-Output: The sum of all multiples of 3 and 5 below `n`
+"""
+Author: Kona Fatsea
+Date: 07/02/2025
 
-1. Initialize sum to 0
-2. For each number from 1 to `n-1`:
-    2.1. If number is divisible by 3 or 5:
-       2.1.1. Add number to sum
+Efficient Iteration (Inclusion - Exclusion) solution for problem-001 of Euler Project.
 
-3. Return sum
-'''
+Problem Statement:
+Find the sum of all multiples of 3 and 5 below a given number n. 
+
+The solutions include:
+1) Brute Force Iteration <------
+2) Efficient Iteration (Inclusion-Exclusion Set Theory) 
+3) Arithmetic Sequence 
+"""
 
 # Good practice to seperate calculation logic into its own function. Helps with testing and maintenance.
 # Main function typically handles user input and output in Python. 
 def sum_multiples(n):
+    """
+    Calculate the sum of multiples of 3 and 5 below n.
+
+    Args: 
+    n (int): Upper limit (exclusive) for considering multiples.
+
+    Returns:
+    int: Sum of all multiples of 3 or 5 below n. 
+    """
     total_sum = 0
+    iterations = 0 # To compare iterations of different methods
     for i in range(1, n): #Iterates until n-1
+        iterations += 1
         if i % 3 == 0 or i % 5 == 0:
             total_sum += i
+    print(f'Number of Iterations: {iterations}')
     return total_sum
 
 def main():
+    """
+    Main function to handle user input and calculate the sum of multiples
+
+    Returns: 
+    tuple: A tuple containing the result and input number.
+    """
     n = int(input("Enter an integer: "))
     result = sum_multiples(n)
     print(f"The sum of multiples of 3 or 5 below {n} is: {result}")
     return result, n
         
-# Ensures the code within the main block runs when the script is run directly and
-# not when imported as a module
-if __name__ == "__main__":
+
+if __name__ == "__main__": # Ensures the code within the main block runs when the script is run directly and not when imported as a module
     result, n = main()
