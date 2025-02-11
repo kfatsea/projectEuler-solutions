61304e2b335bd5871b38b3e392938c2ab3a0eef6
kfatsea <fatseakona@gmail.com>
2025-02-11 11:34:53 +0100

    feat(problem-001/java): Initialize Arithmetic Sequence Approach in Java
    	1. Add class structure
    	2. Create placeholder methods for sumMultiples and main
    	   methods.

---
+++ problem-001/initial_solution/java/ArithmeticProgression.java
@@ -0,0 +1,34 @@
+/*
+Author: Kona Fatsea
+Date: 11/02/2025
+
+Solve problem-001 of Euler Project to find sum of multiples of 3 or 5 below 
+a given number n using the formula for the sum of an arithmetic series.  
+*/ 
+
+public class ArithmeticProgression {
+
+    /**
+     * Entry point of the program. Handles user interaction and boundary checks.
+     * Calls the sumMultiples method to calculate and display the sum of multiples 
+     * of 3 or 5.
+     * 
+     * @param args Command-line arguments (not used in this program).
+     */
+    
+    publlic static void main (String[] args){
+
+    }
+
+    /**
+     * Calculates the sum of multiples of 3 or 5 below a given number n. 
+     * Uses the formula for the sum of an arithmetic progression. 
+     * 
+     * @param n The upper limit (exclusive) for the multiples. 
+     * @return The sum of multiples of 3 or 5 below n 
+     */
+    static int sumMultiples(int n){
+
+    }
+
+}
